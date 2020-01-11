5a5b1b6eb936834e6b0c5ef38a8712fcbc26f9e0
Matt Dambra <mpdambra@gmail.com>
2020-01-11 10:35:24 -0500

    pushing finished code for onThisDay tile

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 $(document).ready(function(e){
 
-    var url = "https://byabbe.se/on-this-day/";
-    var date = moment().format("M/D");
-    var query = url + date + "/events.json";
-    var fact = {
-        date: "",
-        year: "",
-        description: "",
-
-        wikipedia: {
-            title: "",
-            link: ""
-        },
-    }
+    /*
+    * today's date
+    */
+    var today = moment().format("M/D");
     
-    console.log(query);
+    onThisDay();
 
-    $.ajax({
-        url: query,
-        method: "GET"
-      }).then(function(data) {
-            console.log("data: " + data);
-            let index = Math.round(Math.random() * data.events.length);
-            console.log("random: " + index);
+    /*
+    * On this Day in History
+    * Finds a fun historical event from this day and populates a tile.
+    */
+    function onThisDay(){
+        let url = "https://byabbe.se/on-this-day/";
+        let date = moment().format("M/D");
+        let query = url + date + "/events.json";
+        
+        //fact object, holds relevant info for todays fact
+        let fact = {
+            date: "",
+            year: "",
+            description: "",
 
-            fact.date = data.date;
-            fact.year = data.events[index].year;
-            fact.description = data.events[index].description;
-            fact.wikipedia.title = data.events[index].wikipedia[0].title;
-            fact.wikipedia.link = data.events[index].wikipedia[1].wikipedia;
+            wikipedia: {
+                title: "",
+                link: ""
+            },
+        }
 
-            console.log("fact: " + fact);
+        console.log(query);
 
-            $(".onThisDayTitle").html(fact.date + ", " + fact.year);
-            $(".onThisDaySubtitle").html(fact.description);
-            $("#wikipediaTitle").html(fact.wikipedia.title);
-            $(".onThisDayContent").html("<a href=\"" + fact.wikipedia.link + "\" target=\"_blank\">Learn more</a>");
-    });
+        $.ajax({
+            url: query,
+            method: "GET"
+            }).then(function(data) {
+                console.log(data);
+                let index = Math.round(Math.random() * data.events.length);
+                console.log("random: " + index);
 
+                fact.date = data.date;
+                fact.year = data.events[index].year;
+                fact.description = data.events[index].description;
+                fact.wikipedia.title = data.events[index].wikipedia[0].title;
+                fact.wikipedia.link = data.events[index].wikipedia[1].wikipedia;
 
+                console.log("fact: " + fact);
+
+                $(".onThisDayTitle").html(fact.date + ", " + fact.year);
+                $(".onThisDaySubtitle").html(fact.description);
+                $("#wikipediaTitle").html(fact.wikipedia.title);
+                $(".onThisDayContent").html("<a href=\"" + fact.wikipedia.link + "\" target=\"_blank\">Learn more</a>");
+        });
+    }
 });
