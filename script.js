ec729df991f73553dd1c4b43c58a7bb874e83b2c
Matt Dambra <mpdambra@gmail.com>
2020-01-10 19:56:33 -0500

    ajax call set up and stored in an object

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 $(document).ready(function(e){
 
+    var url = "https://byabbe.se/on-this-day/";
+    var date = moment().format("M/D");
+    var query = url + date + "/events.json";
+    var fact = {
+        date: "",
+        year: "",
+        description: "",
+
+        wikipedia: {
+            title: "",
+            link: ""
+        },
+    }
     
+    console.log(query);
+
+    $.ajax({
+        url: query,
+        method: "GET"
+      }).then(function(data) {
+            console.log(data);
+            let index = Math.round(Math.random() * data.events.length);
+            console.log("index = " + index);
+
+            fact.date = data.date;
+            fact.year = data.events[index].year;
+            fact.description = data.events[index].description;
+            fact.wikipedia.title = data.events[index].wikipedia[0].title;
+            fact.wikipedia.link = data.events[index].wikipedia[1].wikipedia;
+
+            console.log(fact);
+
+            
+    });
+
+
 });
