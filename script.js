7db19aa913c84f47463a5fdc7568077fa350e66a
Paul Kirschner <p.kirschner01@gmail.com>
2020-01-11 13:04:10 -0500

    PROGRESS!! able to get article titles and urls to show in html page.  Need to focus on layout now

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function(e){
     var today = moment().format("M/D");
     
     onThisDay();
+    news();
 
     /*
     * On this Day in History
@@ -53,3 +54,98 @@ $(document).ready(function(e){
         });
     }
 });
+
+
+
+///////////////////////////////////////////////////////
+//p=pauls playground
+
+function news(){
+ // This is our API key
+ var APIKey = "Aw4YgVC7dO9qpiArPu0fvwznQjb15ut7";
+
+ // Here we are building the URL we need to query the database
+ var queryURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=" + APIKey;
+
+ var articles = [
+   {
+     title: "",
+     url: ""
+   },
+   {
+     title: "",
+     url: ""
+   },
+   {
+     title: "",
+     url: ""
+   },
+   {
+     title: "",
+     url: ""
+   },
+   {
+     title: "",
+     url: ""
+   }
+ ];
+
+ 
+ // Here we run our AJAX call to the OpenWeatherMap API
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ })
+   // We store all of the retrieved data inside of an object called "response"
+   .then(function(response) {
+
+     // Log the queryURL
+     console.log(queryURL);
+
+     // Log the resulting object
+     console.log(response)
+     //articles[0].title = response.results[0].title;
+     //articles[0].url = response.results[0].url;
+     //article[1].title = response.results[1].title;
+     //article[1].url = response.results[1].url;
+     //article[2].title = respone.results[2].title;
+     //articles[2].url = response.results[2].url;
+
+     for (i=0; i < articles.length; i ++){
+       articles[i].title = response.results[i].title;
+       articles[i].url = response.results[i].url;
+        console.log(articles[i]);
+     }
+
+     $(".newstitle1").text(articles[0].title);
+     $(".newsurl1").text(articles[0].url);
+     $(".newstitle2").text(articles[1].title);
+     $(".newsurl2").text(articles[1].url);
+     $(".newstitle3").text(articles[2].title);
+     $(".newsurl3").text(articles[2].url);
+     $(".newstitle4").text(articles[3].title);
+     $(".newsurl4").text(articles[3].url);
+     $(".newstitle5").text(articles[4].title);
+     $(".newsurl5").text(articles[4].url);
+     
+
+     
+
+     // Transfer content to HTML
+     //$(".first").text(response.results[1]);
+     //$(".wind").text("Wind Speed: " + response.wind.speed);
+     //$(".humidity").text("Humidity: " + response.main.humidity);
+     //$(".temp").text("Temperature (F) " + response.main.temp);
+
+     // Converts the temp to Kelvin with the below formula
+    // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+     //$(".tempF").text("Temperature (Kelvin) " + tempF);
+
+     // Log the data in the console as well
+     //console.log(response.results[0]);
+     //console.log(JSON.stringify(response.results[2]));
+     //console.log("Temperature (F): " + response.main.temp);
+   });
+}
+
+
