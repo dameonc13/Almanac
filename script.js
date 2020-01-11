5a2f1187639f57936e1b0a863d4489c107f62306
Carolene Wilson <carolenesw@gmail.com>
2020-01-11 12:20:52 -0500

    add html elements, js varibales and date value

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-$(document).ready(function(e){
+$(document).ready(function (e) {
 
     /*
     * today's date
     */
     var today = moment().format("M/D");
-    
+
     onThisDay();
 
     /*
     * On this Day in History
     * Finds a fun historical event from this day and populates a tile.
     */
-    function onThisDay(){
+    function onThisDay() {
         let url = "https://byabbe.se/on-this-day/";
         let date = moment().format("M/D");
         let query = url + date + "/events.json";
-        
+
         //fact object, holds relevant info for todays fact
         let fact = {
             date: "",
@@ -33,23 +33,42 @@ $(document).ready(function(e){
         $.ajax({
             url: query,
             method: "GET"
-            }).then(function(data) {
-                console.log(data);
-                let index = Math.round(Math.random() * data.events.length);
-                console.log("random: " + index);
+        }).then(function (data) {
+            console.log(data);
+            let index = Math.round(Math.random() * data.events.length);
+            console.log("random: " + index);
 
-                fact.date = data.date;
-                fact.year = data.events[index].year;
-                fact.description = data.events[index].description;
-                fact.wikipedia.title = data.events[index].wikipedia[0].title;
-                fact.wikipedia.link = data.events[index].wikipedia[1].wikipedia;
+            fact.date = data.date;
+            fact.year = data.events[index].year;
+            fact.description = data.events[index].description;
+            fact.wikipedia.title = data.events[index].wikipedia[0].title;
+            fact.wikipedia.link = data.events[index].wikipedia[1].wikipedia;
 
-                console.log("fact: " + fact);
+            console.log("fact: " + fact);
 
-                $(".onThisDayTitle").html(fact.date + ", " + fact.year);
-                $(".onThisDaySubtitle").html(fact.description);
-                $("#wikipediaTitle").html(fact.wikipedia.title);
-                $(".onThisDayContent").html("<a href=\"" + fact.wikipedia.link + "\" target=\"_blank\">Learn more</a>");
+            $(".onThisDayTitle").html(fact.date + ", " + fact.year);
+            $(".onThisDaySubtitle").html(fact.description);
+            $("#wikipediaTitle").html(fact.wikipedia.title);
+            $(".onThisDayContent").html("<a href=\"" + fact.wikipedia.link + "\" target=\"_blank\">Learn more</a>");
         });
     }
+
+    let alertElement = $(".alert");
+    let weatherIcon = $(".weather-icon");
+    let tempValue = $(".temperature-value");
+    let tempDescription = $(".temperature-description");
+    let locationElement = $(".location");
+    let dateElemenet = $(".date");
+
+
+    var d = moment().format('LLLL');
+    $(".date").append(d);
+    // const weather = {
+    //     temperature: {
+    //         value: 18,
+    //         unit: "fahrenheit"
+    //     }, 
+    // }
+
+
 });
